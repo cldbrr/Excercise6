fc75606e4cd220b66c0c83f5d944730b8fba2d9c
Tomasz Majewski <tmajewski@live.com>
2017-11-28 23:12:44 +0100

    Add MVP Code
    You can tell whether given number is a palindrom or not

---
+++ Exercise6/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Exercise6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercise6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult CheckPalindrome()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CheckPalindrome(Palindrome palindrome)
+        {
+            if (ModelState.IsValid)
+            {
+                if (palindrome.IsPalindrome)
+                {
+                    TempData["ResultMessage"] = string.Format("Given number {0} is a palindrome", palindrome.PalindromeValue);
+                }
+                else
+                {
+                    TempData["ResultMessage"] = string.Format("Given number {0} is NOT a palindrome", palindrome.PalindromeValue);
+                }
+
+                return RedirectToAction("CheckPalindrome");
+
+            }
+
+            return View(palindrome);
+        }
+
+    }
+}
---
+++ Exercise6/Models/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Exercise6.Models
+{
+    public class Palindrome
+    {
+        [Display(Name = "Number")]
+        [Range(1, long.MaxValue, ErrorMessage = "The value must be greater than 0")]
+        public long PalindromeValue { get; set; }
+
+        public bool IsPalindrome
+        {
+            get
+            {
+                string palindromeString = this.PalindromeValue.ToString();
+                int palindromeLength = palindromeString.Length;
+
+                for (int i = 0; i < palindromeLength; i++)
+                {
+                    if (palindromeString[i] != palindromeString[palindromeLength - 1 - i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
